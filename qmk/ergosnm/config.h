f07a6cce306ca52ad99ab3a7425fb226cfda2e95
ZiTe <honmonoh@gmail.com>
2022-12-08 19:40:19 +0800

    Update to match the layout and config of ErgoSNM Rev1.0

--- qmk/ergosnm/config.h
+++ qmk/ergosnm/config.h
@@ -11,33 +11,36 @@
 
 /* key matrix size */
 #define MATRIX_ROWS (5 * 2)
-#define MATRIX_COLS (6)
+#define MATRIX_COLS (7)
 
 /* Key matrix pins. */
-#define MATRIX_ROW_PINS { D3, D2, D1, D4, C6 }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B4, B5 }
+#define MATRIX_ROW_PINS { D3, D2, D4, C6, D7 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, E6, B4, B5 }
+#define MATRIX_ROW_PINS_RIGHT { D3, D2, D4, C6, D7 }
+#define MATRIX_COL_PINS_RIGHT { F4, F5, F6, F7, E6, B4, B0 }
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-#define ENCODERS_PAD_A { D7 }
-#define ENCODERS_PAD_B { E6 }
-#define ENCODERS_PAD_A_RIGHT { D7 }
-#define ENCODERS_PAD_B_RIGHT { E6 }
-#define ENCODER_RESOLUTION 4
+#define ENCODERS_PAD_A { D0 }
+#define ENCODERS_PAD_B { B0 }
+#define ENCODER_RESOLUTIONS { 4 }
+#define ENCODERS_PAD_A_RIGHT { D0 }
+#define ENCODERS_PAD_B_RIGHT { B5 }
+#define ENCODER_RESOLUTIONS_RIGHT { 4 }
 // #define ENCODER_DIRECTION_FLIP
 
 /* Pointing device */
 #define PMW33XX_CS_PIN B6
 #define PMW33XX_CPI 1200 // The CPI range is 100-12000, in increments of 100. Defaults to 1600 CPI.
 #define POINTING_DEVICE_INVERT_Y
+#define POINTING_DEVICE_COMBINED
 
 /* Split keyboard */
 #define MASTER_LEFT
 // #define MASTER_RIGHT
-#define SOFT_SERIAL_PIN D0
+#define SOFT_SERIAL_PIN D1
 #define SPLIT_POINTING_ENABLE
-#define POINTING_DEVICE_COMBINED
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
